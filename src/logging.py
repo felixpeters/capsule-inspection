34b9ce1642a64cd7b9b7fe7b8c63e12b728ea415
Felix Peters <felix@felixpeters.me>
2021-10-25 12:45:18 +0200

    add filename to data tables

--- src/logging.py
+++ src/logging.py
@@ -17,16 +17,19 @@ def create_data_table(path: Path) -> wandb.Table:
     pos_imgs = [Image.open(f) for f in (path/"positive/data").iterdir()]
     pos_masks = [Image.open(f) for f in (path/"positive/annotation").iterdir()]
     neg_imgs = [Image.open(f) for f in (path/"negative/data").iterdir()]
+    pos_fns = [f.name for f in (path/"positive/data").iterdir()]
+    neg_fns = [f.name for f in (path/"negative/data").iterdir()]
     img_size = neg_imgs[0].size
     neg_masks = [Image.new("RGB", img_size) for _ in neg_imgs]
 
     ids = list(range(len(pos_imgs) + len(neg_imgs)))
+    fns = pos_fns + neg_fns
     imgs = pos_imgs + neg_imgs
     masks = pos_masks + neg_masks
     labels = ["positive"] * len(pos_imgs) + ["negative"] * len(neg_imgs)
-    data = [[id, wandb.Image(img), wandb.Image(mask), label] for (
-        id, img, mask, label) in zip(ids, imgs, masks, labels)]
+    data = [[id, fn, wandb.Image(img), wandb.Image(mask), label] for (
+        id, fn, img, mask, label) in zip(ids, fns, imgs, masks, labels)]
 
-    columns = ["id", "image", "mask", "label"]
+    columns = ["id", "filename", "image", "mask", "label"]
     table = wandb.Table(data=data, columns=columns)
     return table
--- tests/logging.py
+++ tests/logging.py
@@ -13,5 +13,5 @@ def data_path():
 
 def test_create_data_table(data_path):
     table = create_data_table(data_path/"capsule")
-    assert len(table.columns) == 4
+    assert len(table.columns) == 5
     assert len(table.data) == 989
