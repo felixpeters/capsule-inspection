9431a917308676ec10c42a01550c4df51d7cccd8
Felix Peters <felix@felixpeters.me>
2021-11-18 11:37:05 +0100

    prepare data in classification module

--- src/data/classification.py
+++ src/data/classification.py
@@ -7,6 +7,16 @@ class SensumClassificationDataModule(LightningDataModule):
     """Handles all things related to data preparation for training classification models."""
 
     def __init__(self, task: str, downloader: DataDownloader, batch_size: int = 64):
+        """[summary]
+
+        Args:
+            task (str): Which dataset to use. Expects one of "capsule" or "softgel".
+            downloader (DataDownloader): Downloader to use for retrieving data.
+            batch_size (int, optional): Batch size to use in data loaders. Defaults to 64.
+
+        Raises:
+            ValueError: If task contains unexpected value.
+        """
         super().__init__()
         self.task = task
         self.downloader = downloader
@@ -15,6 +25,10 @@ class SensumClassificationDataModule(LightningDataModule):
         if self.task not in ["softgel", "capsule"]:
             raise ValueError(
                 f"Task {self.task} is not supported for this dataset.")
+
+    def prepare_data(self):
+        """Retrieves data and saves path to raw data."""
+        self.data_dir = self.downloader.download()
 
     def setup(self):
         pass
--- tests/data/classification.py
+++ tests/data/classification.py
@@ -1,3 +1,27 @@
+import pytest
+
 from src.data.classification import SensumClassificationDataModule
-from src.data.download import URLDownloader
-from src.config import URLs
+
+from .download import sensum_downloader
+
+
+@pytest.fixture
+def classification_data_module(sensum_downloader):
+    return SensumClassificationDataModule("capsule", sensum_downloader, batch_size=8)
+
+
+def test_valid_init(sensum_downloader):
+    dm = SensumClassificationDataModule("capsule", sensum_downloader)
+    assert dm.batch_size == 64
+    assert dm.task == "capsule"
+
+
+def test_invalid_init(sensum_downloader):
+    with pytest.raises(ValueError):
+        dm = SensumClassificationDataModule("invalid", sensum_downloader)
+
+
+def test_prepare_data(classification_data_module):
+    classification_data_module.prepare_data()
+    assert classification_data_module.data_dir.exists()
+    assert (classification_data_module.data_dir/"capsule").exists()
